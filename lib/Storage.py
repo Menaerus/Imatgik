05ec7e7274c70f8249a0a8f0297627f37560ea81
David <david@mannes-tech.com>
2024-03-01 14:49:12 +0100

    Added Storage tests

--- lib/Storage.py
+++ lib/Storage.py
@@ -7,25 +7,28 @@ class SimpleStorage:
     if not os.path.exists(self.storageroot):
       os.mkdir(self.storageroot)
     
-
-
-  def Store(self, username, file):
-    originalfilename = file.filename
-    filename = GenerateStoragename(self, username, file.filename)
-    file.save(filename)
-    return (originalfilename, filename)
-  
-  def Remove(self, filename):
-    os.remove(ospath.join(self.storageroot, filename))
-
   def GenerateStoragename(self, username, filename):
     userstoragename = os.path.join(self.storageroot, username)
     if not os.path.exists(userstoragename):
       os.mkdir(userstoragename)
     
     return os.path.join(userstoragename, filename)
+
+
+  def Store(self, username, file):
+    originalfilename = file.filename
+    (_, simplefilename) = os.path.split(file.filename)
+    filename = self.GenerateStoragename(username, simplefilename)
+    file.save(filename)
+    return (originalfilename, filename)
+  
+  def Remove(self, filename):
+    fullname = os.path.join(self.storageroot, filename)
+    if os.path.exists(fullname): os.remove(fullname)
+
     
   def GetFilenames(self, username):
     userstoragename = os.path.join(self.storageroot, username)
-    return os.listdir(userstoragename)
+    if os.path.exists(userstoragename): return os.listdir(userstoragename)
+    return []
   
--- tests/StorageTests.py
+++ tests/StorageTests.py
@@ -1,3 +1,4 @@
+from werkzeug.datastructures import FileStorage
 import unittest
 import datetime as date
 import sys
@@ -6,6 +7,8 @@ from Storage import *
 from Config import *
 import os
 from TestUtils import *
+from shutil import rmtree
+from filecmp import cmp
 
 simplejson = """
 {
@@ -17,16 +20,33 @@ simpleconfig = SimpleConfig("testconfig.json")
 
 class TestStorage(unittest.TestCase):
   def test_ifstoragecreatestheimagesfolder(self):
-    if os.path.exists("images/david"): os.rmdir("images/david")
-    if os.path.exists("images"): os.rmdir("images")
+    if os.path.exists("images"): rmtree("images")
     storage = SimpleStorage(simpleconfig)
     self.assertTrue(os.path.exists("images"))
 
   def test_storagefoldernamegeneration(self):
-     if os.path.exists("images/david"): os.rmdir("images/david")
+     if os.path.exists("images/david"): rmtree("images/david")
      storage = SimpleStorage(simpleconfig)
      self.assertEqual(os.path.join(os.path.join("images", "david"), "imatge.jpg"), storage.GenerateStoragename("david", "imatge.jpg"))
-     self.assertTrue(os.path.exists("images/david"))
+     self.assertTrue(os.path.exists(os.path.join("images", "david")))
+
+
+  def test_upload(self):
+     if os.path.exists("images/david"): rmtree("images/david")
+     storage = SimpleStorage(simpleconfig)
+     with open(os.path.join("testimages", "imatge1.png"), 'rb') as fp:
+       file = FileStorage(fp)
+       (original, new) = storage.Store("david", file)
+
+     self.assertTrue(os.path.exists(new))
+     self.assertTrue(cmp(original, new))
+     files = storage.GetFilenames("david")
+     self.assertEqual(len(files), 1)
+     self.assertEqual(files[0], "imatge1.png")
+     files = storage.GetFilenames("pepe")
+     self.assertEqual(len(files), 0)
+
+     storage.Remove(new)
 
 if __name__ == '__main__':
     unittest.main()
